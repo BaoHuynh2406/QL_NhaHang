f308f630a97b09a1a01ec5729af51d58a1502816
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2023-11-15 17:15:10 +0700

    Nhập dữ liệu vào database

---
+++ Database/InsertData.sql
@@ -0,0 +1,13 @@
+﻿
+-- Dữ liệu mẫu
+select * from Role
+
+insert into Role VALUES ('MG', N'Quản Lý');
+insert into Role VALUES ('SV', N'Phục vụ');
+insert into Role VALUES ('CS', N'Thu ngân');
+insert into Role VALUES ('KS', N'Bếp');
+go
+
+
+select * FROM Employees
+insert into Employees (FullName, [Password], Email, ID_role) VALUES (N'Admin BaoHuynh', 'admin123', 'huynhbaomts2004@gmail.com', 'MG');
