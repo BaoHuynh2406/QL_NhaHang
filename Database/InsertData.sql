984d098e9dbd3108f6496d9a3deb1365e60c404e
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2023-11-18 11:33:10 +0700

    no message

--- Database/InsertData.sql
+++ Database/InsertData.sql
@@ -1,4 +1,4 @@
-﻿
+﻿use restaurantData;
 -- Dữ liệu mẫu
 select * from Role
 
@@ -11,3 +11,5 @@ go
 
 select * FROM Employees
 insert into Employees (FullName, [Password], Email, ID_role) VALUES (N'Admin BaoHuynh', 'admin123', 'huynhbaomts2004@gmail.com', 'MG');
+
+update Employees set Password = '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9' where Password = 'admin123'
